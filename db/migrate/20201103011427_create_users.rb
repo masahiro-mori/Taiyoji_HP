79ee6b24cd7d9a4e0230739cb25ffbec6b8c810a
EC2 Default User <ec2-user@ip-172-31-37-140.ap-northeast-1.compute.internal>
2020-11-03 05:34:11 +0000

    名前のカラムを分けた

--- db/migrate/20201103011427_create_users.rb
+++ db/migrate/20201103011427_create_users.rb
@@ -1,12 +1,15 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.integer   :date
-      t.string    :name_kanji
-      t.string    :name_kana
-      t.integer   :birth
-      t.integer   :phonenumber
-      t.string    :mailaddress
+      
+      t.date    :book_date
+      t.string  :name_first_kanji
+      t.string  :name_last_kanji
+      t.string  :name_first_kana
+      t.string  :name_last_kana
+      t.date    :birthday
+      t.string  :phonenumber
+      t.string  :mailaddress
       
       
       t.timestamps
